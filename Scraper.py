1cbbe59112805fa7597ad62aa377dc4bd847808f
Rijoeben <ruben.laureys2707@gmail.com>
2021-02-16 14:29:53 +0100

    third

--- Scraper.py
+++ Scraper.py
@@ -1,38 +1,47 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+import time
 
-url = "https://www.blockchain.com/btc/unconfirmed-transactions"
 
-page = requests.get(url)
+while True:
+    url = "https://www.blockchain.com/btc/unconfirmed-transactions"
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    page = requests.get(url)
 
-results = soup.find('div',class_="sc-20ch6p-0 beTSoK")
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-hashes = []
-times = []
-btc =  []
-usd = []
+    results = soup.find('div',class_="sc-20ch6p-0 beTSoK")
 
-for element in results:
-    for i in element:
-        if("Hash" in i.text):
-            hashes.append(i.text[4:])
-        if("Time" in i.text):
-            times.append(i.text[4:])
-        if("Amount (BTC)" in i.text):
-            btc.append(i.text[12:])
-        if("Amount (USD)" in i.text):
-            s = i.text[13:-3]
-            f = float(s)
-            usd.append(s)
+    hashes = []
+    times = []
+    btc =  []
+    usd = []
 
-d = {'Hash': hashes, 'Time': times, 'Amount (BTC)': btc, 'Amount (USD)':usd}
-df = pd.DataFrame(data=d)
+    for element in results:
+        for i in element:
+            if("Hash" in i.text):
+                if i.text[4:] != '':
+                    hashes.append(i.text[4:])
+            if("Time" in i.text):
+                if i.text[4:] != '':
+                    times.append(i.text[4:])
+            if("Amount (BTC)" in i.text):
+                if i.text[12:] != '':
+                    btc.append(i.text[12:])
+            if("Amount (USD)" in i.text):
+                s = i.text[13:-3]
+                if s != '':
+                    l = s.replace(",","")
+                    f = float(l)
+                    usd.append(f)
+                
 
-print(df)
+    d = {'Hash': hashes, 'Time': times, 'Amount (BTC)': btc, 'Amount (USD)':usd}
+    df = pd.DataFrame(data=d)
 
-#maxvalue = df['Amount (USD)'].idxmax()
+    maxvalue = df['Amount (USD)'].idxmax()
 
-#print(df[maxvalue])
+    print(df.loc[[maxvalue]])
+    time.sleep(60)
+
