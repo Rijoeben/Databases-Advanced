2fea834c4b86bcf1fc1383e5edaf741e1ab19377
Rijoeben <ruben.laureys2707@gmail.com>
2021-03-03 16:33:46 +0100

    seven

--- Filter.py
+++ Filter.py
@@ -6,6 +6,10 @@ import pymongo as mongo
 import redis
 import json
 
+#Mijn filter gaat ervanuit dat hij wordt gestart wanneer het scrapen stopt. Zodat ik dus alle data uit 1 scrape sessie kan behandelen en doorsturen naar de mongodb
+client = mongo.MongoClient("mongodb://127.0.0.1:27017")
+bitcoin_db = client["bitcoins"]
+col_high = bitcoin_db["Highest"]
 
 r = redis.Redis(host='localhost',port=6379, db=0)
 
@@ -22,3 +26,19 @@ for x in data:
 df = pd.concat(frameList, ignore_index = True)
 
 print(df)
+
+uniqueTimes = df['Time'].unique()
+
+for time in uniqueTimes:
+    df1 = df.where(df['Time'] == time)
+    maxvalue = df1['Amount (USD)'].idxmax()
+    df2 = df1.loc[[maxvalue]]
+
+
+    max_hash = df2["Hash"].item()
+    max_time = df2["Time"].item()
+    max_btc = df2["Amount (BTC)"].item()
+    max_usd = df2["Amount (USD)"].item()
+
+    data = { "Hash": max_hash, "Time": max_time, "Amount (BTC)": max_btc, "Amount (USD)": max_usd}
+    col_high.insert_one(data)
--- Scraper.py
+++ Scraper.py
@@ -9,10 +9,6 @@ r = redis.Redis(host='localhost',port=6379, db=0)
 
 r.delete("Scrapes")
 
-client = mongo.MongoClient("mongodb://127.0.0.1:27017")
-bitcoin_db = client["bitcoins"]
-col_high = bitcoin_db["Highest"]
-
 def scrape():
 
     while True:
